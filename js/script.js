ac5d56718d004ac662a1920867c5f44cd9f9de30
RadlOx <137636057+RadlOx@users.noreply.github.com>
2024-10-02 15:36:52 +0200

    test

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const logo = document.getElementById ('logo');
+const text = document.getElementById ('text_move');
+const text2 = document.getElementById ('text2_move');
+
+window.addEventListener("load", (event) => {
+    logo.style.opacity = '1';
+    text.style.transform = 'translateX(0)';
+    text2.style.transform = 'translateX(0)';
+  });
+  
